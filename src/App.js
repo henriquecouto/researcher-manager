d6ad61b46f5d266bcb91fe08c493b9bdf600ab38
Henrique Couto <henrique.coutu@live.com>
2019-06-27 23:29:22 -0300

    Material-UI added / AppBar and Description created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+import Description from "./components/Description";
+
+const useStyle = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center"
+  }
+}));
 
 function App() {
+  const classes = useStyle();
+
   return (
-    <h1>Hello, World!</h1>
+    <>
+      <Header title="Cadastro de Pesquisadores" />
+      <Grid container className={classes.root}>
+        <Grid item xs={11}>
+          <Description />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button, Grid } from "@material-ui/core";
+import { Person as PersonIcon, Search as SearchIcon } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1
+  }
+});
+
+export default function Header(props) {
+  const { title } = props;
+
+  const classes = useStyles();
+
+  const log = name => () => {
+    console.log(name);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Grid container justify="space-between" alignItems="center">
+            <Grid item>
+              <Typography variant="h6" color="inherit">
+                {title}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button onClick={log("areaPesquisador")}>
+                <PersonIcon />
+                Área do Pesquisador
+              </Button>
+              <Button onClick={log("buscarPesquisador")}>
+                <SearchIcon />
+                Buscar Pesquisador
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
